6ba8f59ff78a59d5995ace18ba809f8b654368f8
whenofficial <fsmitchm@gmail.com>
2018-10-08 13:56:02 +0400

    add SR command

--- src/commands/sr.js
+++ src/commands/sr.js
@@ -1,3 +1,20 @@
 exports.run = function(client, message, args){
-    message.reply('test')
+    
+    const overstat = require('overstat')
+ 
+    const ow = overstat()
+
+    let btag = args[0].replace('#', '-');
+    
+    message.channel.send(`Finding current SR for **${btag.replace('-', '#')}**...`).then(msg => {
+        ow.playerStats(btag).then(player => {
+            msg.delete();
+            if(isNaN(player.stats.competitiveRank)){
+                message.reply('that BattleTag does not exist **OR** the player has not completed their placement matches yet.');
+                return;
+            }
+            message.channel.send(`**${player.name.replace('-', '#')}**: ${player.stats.competitiveRank} SR`);
+        })
+    })
+
 }
